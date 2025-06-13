18b2e7723aa4954b62602cedfb07b58857c21ec8
HelvetiQuant_AIO <ric.gaetti@pm.me>
2025-06-13 23:16:00 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,10 @@
-from flask import Flask, jsonify, send_from_directory
-import random, time, os
+from flask import Flask, jsonify
+import random
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return send_from_directory('frontend_live', 'index.html')
-
-@app.route('/script.js')
-def script():
-    return send_from_directory('frontend_live', 'script.js')
-
-@app.route('/price')
+@app.route("/price")
 def price():
     return jsonify({
         "token": "XRPUSDT",
@@ -19,6 +12,5 @@ def price():
         "timestamp": int(time.time())
     })
 
-if __name__ == '__main__':
-    port = int(os.getenv("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
