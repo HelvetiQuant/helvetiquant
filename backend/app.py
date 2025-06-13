4a8b6a04ad2da3600bbdf81c0e2d81a6ad9b595b
HelvetiQuant_AIO <ric.gaetti@pm.me>
2025-06-13 18:39:03 +0200

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,20 @@
-# app.py
+from flask import Flask, send_from_directory
+import os
 
-Contenuto placeholder da helvetiquant.
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "HelvetiQuant è attivo"
+
+@app.route("/dashboard")
+def dashboard():
+    return send_from_directory(os.path.join(os.getcwd(), 'frontend_live'), 'index.html')
+
+@app.route("/js")
+def js():
+    return send_from_directory(os.path.join(os.getcwd(), 'frontend_live'), 'script.js')
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
